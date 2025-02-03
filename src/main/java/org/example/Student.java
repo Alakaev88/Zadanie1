765335b69ac0fe2e9eb83fc2e300c28c108c336c
Алакаев Булат <pakitos02@mail.ru>
2025-02-03 16:05:48 +0500

    добавил классы Student и StudentList

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,19 +1,52 @@
 package org.example;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
     private int id;
     private String name;
 
+    // Конструктор без параметров (обязательно для Jackson)
     public Student() {}
 
-    public Student(int id, String name){
+    public Student(int id, String name) {
         this.id = id;
         this.name = name;
     }
-    public int getId(){
-        return id;
-    }
-    public String getName(){
-        return name;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "'}";
     }
 }
+
+
+
+
+
+
+
+//public class Student {
+//    private int id;
+//    private String name;
+//
+//    public Student() {}
+//
+//    public Student(int id, String name){
+//        this.id = id;
+//        this.name = name;
+//    }
+//    public int getId(){
+//        return id;
+//    }
+//    public String getName(){
+//        return name;
+//    }
+//}
--- src/main/java/org/example/StudentList.java
+++ src/main/java/org/example/StudentList.java
@@ -1,16 +1,41 @@
 package org.example;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StudentList {
     private List<Student> students;
 
     public StudentList(){}
 
-    public StudentList(List<Student> students){
-        this.students = students;
-    }
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return students;
     }
+    public void setStudents(List<Student> students){
+        this.students = students;
+    }
+    @Override
+    public String toString(){
+        return "StudentLisn{" +"students=" + students+ '}';
+    }
 }
+
+
+
+
+
+//import java.util.List;
+//
+//public class StudentList {
+//    private List<Student> students;
+//
+//    public StudentList(){}
+//
+//    public StudentList(List<Student> students){
+//        this.students = students;
+//    }
+//    public List<Student> getStudents(){
+//        return students;
+//    }
+//}
