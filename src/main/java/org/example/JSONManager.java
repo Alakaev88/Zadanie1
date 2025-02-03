daa64f5c6aef2d60a778780ceb6efe380953cfc9
Алакаев Булат <pakitos02@mail.ru>
2025-02-03 16:59:04 +0500

    добавил два метода в JSONManager поиск по id и name

--- src/main/java/org/example/JSONManager.java
+++ src/main/java/org/example/JSONManager.java
@@ -3,6 +3,7 @@ package org.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class JSONManager {
     private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -14,5 +15,18 @@ public class JSONManager {
             System.out.println("Ошибка чтения JSON: "+ e.getMessage());
             return new StudentList(); // если ошибка, вернем пустой список
         }
+    }
+    //метод поиска по id
+    public static Student findStudentById(StudentList studentList, int id){
+        return studentList.getStudents().stream()
+                .filter(student -> student.getId() == id)
+                .findFirst()
+                .orElse(null); // если не найдено, возвращаем null
+    }
+    //метод поиска по name
+    public static List<Student> findStudentByName(StudentList studentList, String name) {
+        return studentList.getStudents().stream()
+                .filter(student -> student.getName().equalsIgnoreCase(name))
+                .toList();// возвращаем список студентов с таким именем
     }
 }
